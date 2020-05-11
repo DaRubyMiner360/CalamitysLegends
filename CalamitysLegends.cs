d44b6f185aa532a3fdc635bf7d0fa412f603a858
DaRubyMiner360 <DaRubyMiner360@gmail.com>
2020-05-11 17:56:05 -0500

    Link to GitHub

---
+++ CalamitysLegends.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace CalamitysLegends
+{
+	public class CalamitysLegends : Mod//-Squideeeeee for doing the sprites for this mod
+	{
+		public CalamitysLegends()
+		{
+		}
+	}
+}
